43e2ab5a10524f45fd91cd69c60215750df763b2
Anubhab Sen <anubhabsen2007@gmail.com>
2026-06-17 19:49:41 +0530

    Reset FTAN
    Reset FTAN jobs to "T" after an unsuccessful run or delete all the done jobs.

---
+++ msnoise_tomo/reset_ftan.py
@@ -0,0 +1,32 @@
+import os
+from msnoise.api import *
+from sqlalchemy import text
+
+def main(all):
+    db = connect()
+    result = db.execute(text("UPDATE jobs SET flag='T' WHERE jobtype='TOMO_FTAN'"))
+    db.commit()
+    deleted=0
+    if all:
+        params = get_params(db)
+        comps = params.components_to_compute
+        filters = get_filters(db)
+        while is_next_job(db, jobtype='TOMO_FTAN'):
+            jobs = get_next_job(db, jobtype='TOMO_FTAN')
+
+            for job in jobs:
+                netsta1, netsta2 = job.pair.split(':')
+                for filter in filters:
+                    for comp in comps:
+                        fn = os.path.join("DISP CURVE PLOTS", "%02i" % filter.ref, comp,
+                                          "%s - %s.png" % (netsta1, netsta2))
+                        if os.path.exists(fn):
+                            os.remove(fn)
+                            deleted+=1
+        result = db.execute(text("UPDATE jobs SET flag='T' WHERE jobtype='TOMO_FTAN'"))
+        db.commit()
+    print(f"{result.rowcount} jobs reset to T")
+    print(f"{deleted} jobs deleted.")
+
+if __name__ == "__main__":
+    main()
